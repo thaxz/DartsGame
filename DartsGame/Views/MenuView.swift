4e8b58090bdedee1a1d456ee5003a04802474512
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-17 19:33:26 -0300

    feature(Assets): Adding missing menuview and assets

--- DartsGame/Views/MenuView.swift
+++ DartsGame/Views/MenuView.swift
@@ -1,21 +1,44 @@
 //
-//  ContentView.swift
-//  DartsGame
+//  MenuView.swift
+//  DartChallenge
 //
-//  Created by thaxz on 17/10/23.
+//  Created by thaxz on 08/10/23.
 //
 
 import SwiftUI
 
 struct MenuView: View {
+    
+    @StateObject private var routeManager = NavigationRouter()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack(path: $routeManager.routes){
+            ZStack {
+                Image("menuBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                VStack(spacing: 20){
+                    Image("logo")
+                        .padding(.top, 70)
+                    Spacer()
+                    PrimaryButton(title: "play") {
+                        routeManager.push(to: .game)
+                    }
+                    SecondaryButton(title: "previous macthes") {
+                        // needs to push matches from core data
+                        routeManager.push(to: .previousMatches(matches: []))
+                    }
+                    Spacer()
+                        .frame(height: 80)
+                }
+                .padding(.horizontal, 20)
+            }
+            .navigationDestination(for: Route.self) { route in
+                route
+            }
         }
-        .padding()
+        .environmentObject(routeManager)
     }
 }
 
