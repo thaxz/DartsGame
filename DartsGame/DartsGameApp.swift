f5de8280ffb6c1b7c362f6a02386ad3fb52cabc2
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-17 19:07:30 -0300

    first commit

---
+++ DartsGame/DartsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsGameApp.swift
+//  DartsGame
+//
+//  Created by thaxz on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DartsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
