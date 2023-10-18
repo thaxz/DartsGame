f98a3bf9e4284ecbe9c12e12b0976890ff150f1e
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-18 16:11:01 -0300

    feature(CoreData): Adjusting throw button

--- DartsGame/Views/GameView.swift
+++ DartsGame/Views/GameView.swift
@@ -29,7 +29,17 @@ struct GameView: View, GameLogicDelegate {
                 headerSection
                 Spacer()
                 dartsLeft
-                trowButton
+                PrimaryButton(title: "throw") {
+                    if viewModel.throwNumber < 5 {
+                        ARManager.shared.actionsStream.send(.placeDart)
+                        viewModel.throwNumber += 1
+                        if viewModel.throwNumber >= 5 {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                viewModel.gameOver()
+                            }
+                        }
+                    }
+                }
             }
             .padding(40)
             if isPaused {
@@ -79,29 +89,6 @@ extension GameView {
             .foregroundColor(.white)
     }
     
-    var trowButton: some View {
-        Button {
-            if viewModel.throwNumber < 5 {
-                ARManager.shared.actionsStream.send(.placeDart)
-                viewModel.throwNumber += 1
-                if viewModel.throwNumber >= 5 {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                        viewModel.gameOver()
-                    }
-                }
-            }
-        } label: {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(Color.theme.primary)
-                Text("trow".uppercased())
-                    .font(.custom("Futura-Medium", size: 22))
-                    .foregroundColor(.white)
-            }
-        }
-        .frame(maxWidth: .infinity)
-        .frame(height: 60)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
