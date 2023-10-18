3b5ec54576d081b3b9a640bea63616ab384684ee
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-18 19:02:17 -0300

    feature(Documentation): Documenting ARView

--- DartsGame/ARViews/GameViewController+Collisions.swift
+++ DartsGame/ARViews/GameViewController+Collisions.swift
@@ -12,12 +12,14 @@ import ARKit
 
 extension GameViewController: SCNPhysicsContactDelegate {
     
+    /// Called when a physics contact occurs in the scene.
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
         if contact.nodeA.physicsBody?.categoryBitMask == CollisionCategory.dartBoard.rawValue || contact.nodeB.physicsBody?.categoryBitMask == CollisionCategory.dartBoard.rawValue {
             print("Hit board!")
-            self.removeNodeWithAnimation(contact.nodeB, isCollision: false) // remove dart
+            self.removeNodeWithAnimation(contact.nodeB, isCollision: false)
             self.addPoint()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { // remove&replace board
+            // Delay and then remove and replace the dartboard
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
                 self.removeNodeWithAnimation(contact.nodeA, isCollision: true)
                 self.addNewBoard()
             })
